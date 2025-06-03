0c308185a71e47ee036b8ed27032921887e8e953
Guilherme <amaroguilherme99@gmail.com>
2025-06-03 15:31:27 -0300

    feat. new updates

--- src/IFCE.TodoList.Infra.Data/Contexts/ApplicationDbContext.cs
+++ src/IFCE.TodoList.Infra.Data/Contexts/ApplicationDbContext.cs
@@ -8,7 +8,7 @@ public class ApplicationDbContext :  DbContext
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) {}
     
-    private readonly string _connectionString = "server=localhost;port=3306;database=ifce_todolist;user=root;password=Lab@inf019;";
+    private readonly string _connectionString = "server=localhost;port=3306;database=ifce_todolist;user=;password=;";
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         if (!optionsBuilder.IsConfigured)
